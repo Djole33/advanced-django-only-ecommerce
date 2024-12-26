4a5fca2e7ecc67738db342a58de592ab2b5e078e
WebWrap <149431380+Djole33@users.noreply.github.com>
2024-12-26 22:44:04 +0100

    'details,_category_and_home_page_edited'

--- main/urls.py
+++ main/urls.py
@@ -3,8 +3,10 @@ from . import views
 
 urlpatterns = [
    path('', views.index, name="index"),
+   path('home/', views.index, name="index"),
    path('shop/', views.shop, name="shop"),
-   path('detail/', views.detail, name="detail"),
+   path('detail/<int:pk>/', views.detail, name="detail"),
+   path('category/<str:name>/', views.category_page, name="category_page"),
    path('cart/', views.cart, name="cart"),
    path('checkout/', views.checkout, name="checkout"),
    path('contact/', views.contact, name="contact"),
--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.conf import settings
-from .models import *
+from .models import Product, Category
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
 
 # Create your views here.
+
+def base(request):
+    categories = Category.objects.all()
+
+    return render(request, 'main/base.html', {'categories': categories})
 
 def index(request):
     products = Product.objects.all()
@@ -15,8 +21,14 @@ def index(request):
 def shop(request):
     return render(request, 'main/shop.html', {'MEDIA_URL': settings.MEDIA_URL})
 
-def detail(request):
-    return render(request, 'main/detail.html', {'MEDIA_URL': settings.MEDIA_URL})
+def detail(request, pk):
+    product = Product.objects.get(id=pk)
+    return render(request, 'main/detail.html', {'product': product, 'MEDIA_URL': settings.MEDIA_URL})
+
+def category_page(request, name):
+    category = Category.objects.get(name=name.capitalize())
+    products = Product.objects.filter(category=category.id)
+    return render(request, 'main/category_page.html', {'category': category, 'products': products, 'MEDIA_URL': settings.MEDIA_URL})
 
 def cart(request):
     return render(request, 'main/cart.html', {'MEDIA_URL': settings.MEDIA_URL})
